7cfe1f4ca68e1439336ac1353f8af15caab4385f
Vkarmane <vkarmanepro@gmail.com>
2025-06-25 17:23:45 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20, fontFamily: 'sans-serif' }}>
+      <h1>ChosenPeople</h1>
+      <p>Welcome to the portal.</p>
+    </div>
+  );
+}
